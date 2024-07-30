f92938bd00bf3ba40099d3010b23155a726aeb2c
Deepak Malpani <deepak.malpani@exxonmobil.com>
2024-07-30 10:32:26 +0530

    chore: Add Header component to homepage

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,14 +1,20 @@
 import Experience from "@/components/Experience";
+import Header from "@/components/Header";
 import { Introduction } from "@/components/Introduction";
 import Skills from "@/components/Skills";
+import { Meteors } from "@/components/ui/meteors";
 
 const Home = () => {
   return (
-    <main className="flex flex-col items-center">
+    <div className="flex flex-col min-h-screen bg-background-dark text-foreground-dark" >
+      <Header/>
+    <main className="flex-1">
       <Introduction />
       <Experience />
       <Skills/>
+      {/* <Meteors number={20} className="z-0"/> */}
     </main>
+    </div>
   );
 };
 
