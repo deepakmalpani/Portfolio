61e1a474ded039d6b211971f71a121eb4ce15acb
Deepak Malpani <deepak.malpani@exxonmobil.com>
2024-08-03 19:44:41 +0530

    refactor: Update Introduction component styling and add contact and resume buttons

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -1,16 +1,26 @@
+"use client";
+
 import React from "react";
-import { cn } from "@/lib/utils";
 import { Spotlight } from "./ui/spotlight";
 
 export function Introduction() {
+  const handleContactClick = () => {
+    window.location.href = "mailto:deepakmalpani0@gmail.com";
+  };
+
+  const handleDownloadResumeClick = () => {
+    const url = "https://drive.google.com/uc?export=download&id=14FTSCtvfdQYnRcnwIvTiqh5LPaCb3jFE";
+    window.open(url, "_blank");
+  };
+
   return (
     <section id="Introduction">
-      <div className="h-[20rem] w-full rounded-md flex md:items-center md:justify-center antialiased relative overflow-hidden">
+      <div className="h-[27rem] w-full rounded-md flex md:items-center md:justify-center antialiased relative overflow-hidden">
         <Spotlight
           className="-top-40 left-0 md:left-60 md:-top-20"
           fill="white"
         />
-        <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
+        <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0 mt-8">
           <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text bg-opacity-50">
             Deepak Malpani <br />
             <span className="text-3xl">Full stack developer</span>
@@ -21,7 +31,21 @@ export function Introduction() {
             systems, I make sure everything runs smoothly. Check out my work,
             and let&apos;s team up to create something legendary!
           </p>
-        </div>        
+          <div className="flex gap-4 justify-center flex-row mt-4">
+            <button
+              className="h-12 animate-shimmer rounded-md border border-primary bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-primary transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+              onClick={handleContactClick}
+            >
+              Contact Me
+            </button>
+            <button
+              className="h-12 animate-shimmer rounded-md border border-input bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-muted-foreground-dark transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+              onClick={handleDownloadResumeClick}
+            >
+              Download Resume
+            </button>
+          </div>
+        </div>
       </div>
     </section>
   );
